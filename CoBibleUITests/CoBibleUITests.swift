bb3ad90fe77d92789dddd56022e8ce3e89b440ac
Tram Anh Vo <tramanh2003us@gmail.com>
2025-04-30 15:10:28 +0200

    test  added

--- CoBibleUITests/CoBibleUITests.swift
+++ CoBibleUITests/CoBibleUITests.swift
@@ -3,10 +3,11 @@
 //  CoBibleUITests
 //
 //  Created by Erwan Weinmann on 03/03/2025.
-//
+// AND created by Trâm Anh VO on 05/04/2025
 
 import XCTest
 
+//Test passed successfully
 final class CoBibleUITests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -22,6 +23,7 @@ final class CoBibleUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+//Test passed successfully
     @MainActor
     func testExample() throws {
         // UI tests must launch the application that they test.
@@ -31,6 +33,7 @@ final class CoBibleUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+//Test "No baseline average for Duration (AppLaunch) , Test was skipped/not run
     @MainActor
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
@@ -41,3 +44,5 @@ final class CoBibleUITests: XCTestCase {
         }
     }
 }
+
+
--- CoBibleUITests/CoBibleUITestsLaunchTests.swift
+++ CoBibleUITests/CoBibleUITestsLaunchTests.swift
@@ -1,12 +1,14 @@
+
 //
 //  CoBibleUITestsLaunchTests.swift
 //  CoBibleUITests
 //
 //  Created by Erwan Weinmann on 03/03/2025.
-//
+// AND created by Trâm Anh VO on 05/04/2025
 
 import XCTest
 
+//Test passed successfully
 final class CoBibleUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
@@ -17,6 +19,7 @@ final class CoBibleUITestsLaunchTests: XCTestCase {
         continueAfterFailure = false
     }
 
+//Test passed successfully
     @MainActor
     func testLaunch() throws {
         let app = XCUIApplication()
@@ -29,5 +32,55 @@ final class CoBibleUITestsLaunchTests: XCTestCase {
         attachment.name = "Launch Screen"
         attachment.lifetime = .keepAlways
         add(attachment)
+    }
+}
+
+
+
+//
+//  CoBibleTests.swift
+//  CoBibleTests
+//
+//  Created by Erwan Weinmann on 03/03/2025.
+//  Edited by Trâm Anh VO on 05/04/2025
+//
+
+import XCTest
+import SwiftData
+@testable import CoBible
+
+//Test passed successfully
+@MainActor
+final class CoBibleTests: XCTestCase {
+
+    var modelContainer: ModelContainer!
+    var context: ModelContext!
+
+    override func setUpWithError() throws {
+        let schema = Schema([
+            Shortcut.self
+        ])
+        let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        modelContainer = try ModelContainer(for: schema, configurations: [config])
+        context = modelContainer.mainContext
+    }
+
+    override func tearDownWithError() throws {
+        modelContainer = nil
+        context = nil
+    }
+    
+//Test passed successfully
+    func testPopulateShortcutsAddsData() async throws {
+        // Ensure the context is empty
+        let initialShortcuts = try context.fetch(FetchDescriptor<Shortcut>())
+        XCTAssertEqual(initialShortcuts.count, 0, "Expected no shortcuts initially")
+
+        // Call your population logic
+        ShortcutDataManager.populateShortcuts(context: context)
+
+        // Fetch again to see if data was added
+        let newShortcuts = try context.fetch(FetchDescriptor<Shortcut>())
+        XCTAssertGreaterThan(newShortcuts.count, 0, "Expected shortcuts to be populated")
     }
 }
