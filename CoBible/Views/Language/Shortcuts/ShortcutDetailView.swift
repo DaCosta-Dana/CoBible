689802468f0d6657b7c7d6e086da57c9fe6e7383
vo-tramanh-unilu <113997730+vo-tramanh-unilu@users.noreply.github.com>
2025-05-05 18:42:21 +0200

    change to light mode

--- CoBible/Views/Language/Shortcuts/ShortcutDetailView.swift
+++ CoBible/Views/Language/Shortcuts/ShortcutDetailView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import SwiftData
 import WebKit
 
-// WebView to show syntax-highlighted code
+// WebView to show syntax-highlighted coding
 struct SyntaxHighlightedWebView: UIViewRepresentable {
     var code: String
     var language: String
